12ad5799e53014768ac73701573d48a8bfd74000
Prateekg0 <144532757+Prateekg0@users.noreply.github.com>
2025-12-05 07:56:35 +0530

    Rename 'ChillMode Call' to 'CallMate'change name of aaplication

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -26,7 +26,7 @@ function HomeComponent() {
 
                 <div style={{ display: "flex", alignItems: "center" }}>
 
-                    <h2>ChillMode Call</h2>
+                    <h2>CallMate</h2>
                 </div>
 
                 <div style={{ display: "flex", alignItems: "center" }}>
